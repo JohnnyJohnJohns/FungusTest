3676203ffa0c389fd73edf76e88c68db1fff4a8c
JohnnyJohnJohns <johnhealy123@gmail.com>
2014-12-02 17:56:22 +0000

    Moved view, added counter

---
+++ UnityFungus/Assets/Scripts/CountScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountScript : MonoBehaviour {
+
+	public int timer = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer++;
+		Debug.Log (timer);
+	
+	}
+}
